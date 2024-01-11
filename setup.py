51d0ead98e90285b1925976342a5cbf72bd0b0e9
long.chen <lipracer@gmail.com>
2024-01-12 00:07:19 +0800

    using python cmake build system (#67)

--- setup.py
+++ setup.py
@@ -1,28 +1,53 @@
-from setuptools import setup, find_packages
-from distutils.core import setup
-import os, sys
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import os
+import subprocess
+from setuptools.extension import Extension as _Extension
+from glob import glob
+import shutil
 
-def sync_shell(cmd):
-    err = os.system(cmd)
-    assert not err, f'exec cmmand:{cmd} fail!'
+class CMakeExtension(_Extension):
+    def __init__(self, name, sourcedir=''):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
-sync_shell("rm -rf build")
-script_dir = os.path.dirname(os.path.abspath(__file__))
-sync_shell(f"cmake -S {script_dir} -B build -GNinja && cmake --build build")
-sync_shell(f"cp -r include build/lib/cuda_mock")
-sync_shell(f"cp -r third_party/fmt/include/fmt build/lib/cuda_mock/include")
-sync_shell(r"cd build/lib && ls | grep -v '\<cuda_mock\>' | xargs -I {} rm -rf {}")
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name))),
+            '-DPYTHON_EXECUTABLE=' + sys.executable,
+            '-DCMAKE_INSTALL_PREFIX=' + os.path.join(os.path.abspath(os.path.dirname(self.build_lib)), os.path.basename(self.build_lib))
+        ]
+
+        build_dir = os.path.abspath(os.path.join(self.build_temp, ext.name))
+        os.makedirs(build_dir, exist_ok=True)
+
+        ninja_args = [
+            '-GNinja',  # Specify Ninja as the generator
+        ]
+
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args + ninja_args, cwd=build_dir)
+        subprocess.check_call(['cmake', '--build', '.'], cwd=build_dir)
+        subprocess.check_call(['ninja', 'install'], cwd=build_dir)
 
 setup(
     name="cuda-mock",
-    version="0.0.7",
+    version="0.0.8",
     author="lipracer",
     author_email="lipracer@gmail.com",
     description="a tools hook some api call at runtime",
 
     url="https://github.com/lipracer/torch-cuda-mock", 
 
-    packages=['cuda_mock'],
-    # include_package_data = True,
-    package_dir={'cuda_mock': 'src/cuda_mock'},
+    ext_modules=[CMakeExtension('cuda_mock_impl', sourcedir='.')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    package_dir={'': 'src'},
+    package_data={'' : ['*']},
+    zip_safe=False,
 )
