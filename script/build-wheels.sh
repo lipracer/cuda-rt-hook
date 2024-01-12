8383bdd0fb03137374ac1f0befcf4e933afa93c4
long.chen <lipracer@gmail.com>
2024-01-12 10:54:08 +0800

    Dev (#69)
    * add build script
    
    * publish support many linux

---
+++ script/build-wheels.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+pip install -r requirements.txt
+python setup.py sdist bdist_wheel
+
+# new_name=`ls dist | grep whl | awk -F'-' '{print $1"-"$2".whl"}'`
+
+# ls dist | grep whl | xargs -I {} mv dist/{} dist/$new_name
