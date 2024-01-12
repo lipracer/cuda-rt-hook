a1310865982cd1415f9fb1500ac9f435762ba171
long.chen <lipracer@gmail.com>
2024-01-12 13:26:13 +0800

    fix publish yaml (#76)

--- script/build-wheels.sh
+++ script/build-wheels.sh
@@ -6,6 +6,6 @@ git submodule update --init --recursive
 pip install -r requirements.txt
 python setup.py sdist bdist_wheel
 
-# new_name=`ls dist | grep whl | awk -F'-' '{print $1"-"$2".whl"}'`
+new_name=`ls dist | grep whl | awk -F'-' '{print $1"-"$2".whl"}'`
 
-# ls dist | grep whl | xargs -I {} mv dist/{} dist/$new_name
+ls dist | grep whl | xargs -I {} mv dist/{} dist/$new_name
