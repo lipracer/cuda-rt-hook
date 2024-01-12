4c773fede623f96a6147154dcca8ea147874fdf9
lipracer <lipracer@gmail.com>
2024-01-12 13:40:20 +0800

    fix build script miss python version

--- script/build-wheels.sh
+++ script/build-wheels.sh
@@ -4,8 +4,11 @@ set -e
 
 # git submodule update --init --recursive
 pip install -r requirements.txt
+
+rm -rf dist
+
 python setup.py sdist bdist_wheel
 
-new_name=`ls dist | grep whl | awk -F'-' '{print $1"-"$2".whl"}'`
+new_name=`ls dist | grep whl | awk -F'-' '{print $1"-"$2"-py3-none-any.whl"}'`
 
 ls dist | grep whl | xargs -I {} mv dist/{} dist/$new_name
