cfff1da4b9ef8970c0dd2f6a2ecf09c9ee1a98cb
long.chen <lipracer@gmail.com>
2024-03-28 14:55:42 +0800

    bump 0.1.4 (#112)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="cuda-mock",
-    version="0.1.3",
+    version="0.1.4",
     author="lipracer",
     author_email="lipracer@gmail.com",
     description="a tools hook some api call at runtime",
