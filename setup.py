c68e8936ac794074e0bd4279b43b8f0d3bac5a0e
Gao Tianlin <gaotianlin@baidu.com>
2024-04-14 21:10:30 +0800

    bump to 0.1.6 (#128)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="cuda-mock",
-    version="0.1.5",
+    version="0.1.6",
     author="lipracer",
     author_email="lipracer@gmail.com",
     description="a tools hook some api call at runtime",
