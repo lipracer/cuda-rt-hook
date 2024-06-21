0855c1b2b0878c59302ee0e55ee88d343347652c
Jianbang Yang <yangjianbang112@gmail.com>
2024-06-21 16:24:11 +0800

    The first official release. Bump version to 1.0.0. (#168)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="cuda-mock",
-    version="0.1.12",
+    version="1.0.0",
     author="lipracer",
     author_email="lipracer@gmail.com",
     description="a tools hook some api call at runtime",
