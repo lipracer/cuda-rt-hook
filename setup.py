936dc59988e37b23e548c243a00749b48167fccc
Jianbang Yang <yangjianbang112@gmail.com>
2024-07-11 14:10:29 +0800

    bump v1.1.0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="cuda-mock",
-    version="1.0.0",
+    version="1.1.0",
     author="lipracer",
     author_email="lipracer@gmail.com",
     description="a tools hook some api call at runtime",
